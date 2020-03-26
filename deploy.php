887da3a669a4c5287ba9c7bfb7942e897779cc4c
Nguyen Van Tien <nguyentien1098@gmail.com>
2020-03-26 17:30:18 +0700

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -7,7 +7,7 @@ require 'recipe/laravel.php';
 set('application', 'zero_downtime_deploy');
 
 // Project repository
-set('repository', 'git@github.com:huongnx-0984/autodeploy_example.git');
+set('repository', 'git@github.com:tiennv-1572/test_deploy.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', false);
